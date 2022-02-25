140a9b3de5e073ff9e03a0a0cf9a7ed1320714dd
Shivani Lohokare <shivanilohokaree@gmail.com>
2022-02-25 16:08:35 +0100

    Set upper limit to stack

--- stack.js
+++ stack.js
@@ -1,18 +1,27 @@
 class stack{
     top = -1;
     stackNum = [];
+    limit = 4;
+    stackLimit = 0;
 
-    constructor(){
+    constructor(stackLimit){
+        this.stackLimit = stackLimit;
     }
 
-    push(num){
+    push = (num) => {
+        if (this.top === this.stackLimit-1)
+        {
+            console.log("Stack size is full!");
+        }
+        else{
         this.top++;
         this.stackNum[this.top] = num;
         console.log("Number pushed into stack");
         console.log(this.stackNum);
+        }
     }
 
-    pop(){
+    pop= () => {
      
             this.stackNum[this.top] = null ; 
             this.top--;
@@ -28,10 +37,15 @@ class stack{
     
 }
 
-let obj = new stack();
+
+let UpperLimit = 10
+let obj = new stack(UpperLimit);
 obj.push(10);
 obj.push(20);
 obj.push(30);
+obj.push(40);
+obj.push(50);
+obj.push(60);
 obj.peek();
 obj.pop();
 obj.peek();
