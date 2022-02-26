4e3ad6b1e98bdb16f144dff24ca2c659e25814fb
Shivani Lohokare <shivanilohokaree@gmail.com>
2022-02-26 23:46:28 +0100

    Merge Implementation

--- stack.js
+++ stack.js
@@ -1,13 +1,12 @@
 class stack{
     top = -1;
     stackNum = [];
-    limit = 4;
     stackLimit = 0;
 
     constructor(stackLimit){
         this.stackLimit = stackLimit;
     }
-
+    
     push = (num) => {
         if (this.top === this.stackLimit-1)
         {
@@ -21,31 +20,52 @@ class stack{
         }
     }
 
-    pop= () => {
-     
+    pop = () => {
+        if(this.top == -1){
+            console.log("Pop method: Stack is empty!");
+        }
+        else{
             this.stackNum[this.top] = null ; 
             this.top--;
             this.stackNum.length = this.stackNum.length-1;
             console.log("Last number removed");
             console.log(this.stackNum);
-    
+        }
     }
 
-    peek(){
+    peek = () => {
+        if(this.top == -1){
+            console.log("Peek method: Stack is empty!");
+        }
+        else{
         console.log("The top element of stack is : "+ this.stackNum[this.top]);
+        }
+    }
+
+    isFull = () => {
+        let size = this.top+1;
+        if (this.top === this.stackLimit-1)
+        {
+            console.log("Stack is full!");
+        }
+        else{
+            console.log("Stack is not full and current size of stack is: " + size + " upper limit of stack is: " + this.stackLimit);
+        }
     }
     
 }
 
 
-let UpperLimit = 10
+let UpperLimit = 10;
 let obj = new stack(UpperLimit);
+
 obj.push(10);
 obj.push(20);
 obj.push(30);
 obj.push(40);
-obj.push(50);
-obj.push(60);
-obj.peek();
-obj.pop();
-obj.peek();
+obj.isFull();
+obj.push(10);
+
+
+
+
